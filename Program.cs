0245cfc75efc78390b4c0ada60fc9026a45dd22d
ArturFoden <fodena@tcd.ie>
2022-09-05 03:20:18 +0100

    complete

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ string computerName = new String(Console.ReadLine());
 
 IPAddress[] ipAddresses = Dns.GetHostAddresses(computerName);
 
-Console.WriteLine("IPv6: " + ipAddresses[0] + "\nIPv4: " + ipAddresses[1]);
+void DisplayData(){
+  try{
+      Console.WriteLine("IPv6: " + ipAddresses[0] + "\nIPv4: " + ipAddresses[1]);
+      Console.ReadKey();
+    }
+    catch(Exception){
+      Console.WriteLine("error");
+    }
+}
 
-Console.ReadKey();
+DisplayData();
