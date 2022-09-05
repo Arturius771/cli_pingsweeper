4da4ff942596c235b671690b72fc2bfba52f35c5
ArturFoden <fodena@tcd.ie>
2022-09-05 11:14:46 +0100

    fixed exception

--- Program.cs
+++ Program.cs
@@ -1,18 +1,23 @@
 ﻿using System.Net;
-
-Console.WriteLine("Enter device/computer name: ");
-
-string computerName = new String(Console.ReadLine());
-
-IPAddress[] ipAddresses = Dns.GetHostAddresses(computerName);
+using System.Net.Sockets;
 
 void DisplayData(){
   try{
+      Console.WriteLine("Enter device/computer name: ");
+
+      string computerName = new String(Console.ReadLine());
+
+      IPAddress[] ipAddresses = Dns.GetHostAddresses(computerName);
+
       Console.WriteLine("IPv6: " + ipAddresses[0] + "\nIPv4: " + ipAddresses[1]);
       Console.ReadKey();
     }
-    catch(Exception ex){
-      Console.WriteLine(ex.ToString());
+    catch(SocketException){
+      Console.WriteLine("No such host is known\nPress 'y' key to try again");
+      if(Console.ReadKey().KeyChar == 'y'){
+        Console.WriteLine();
+        DisplayData();
+      }
     }
 }
 
