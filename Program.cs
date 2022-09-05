bdf3d3eafbf4a3efc2b7e3febfb44dc582188ae6
ArturFoden <fodena@tcd.ie>
2022-09-05 03:01:41 +0100

    final version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+
+Console.WriteLine("Enter device/computer name: ");
+
+string computerName = new String(Console.ReadLine());
+
+IPAddress[] ipAddresses = Dns.GetHostAddresses(computerName);
+
+Console.WriteLine("IPv6: " + ipAddresses[0] + "\nIPv4: " + ipAddresses[1]);
+
+Console.ReadKey();
