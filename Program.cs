67a515e35767eaacdec2fd47886a71e537ff36a3
ArturFoden <fodena@tcd.ie>
2022-09-07 14:53:55 +0100

    refactored

--- Program.cs
+++ Program.cs
@@ -1,24 +1,19 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+try{
+    Console.WriteLine("Enter device/computer name: ");
 
-void DisplayData(){
-  try{
-      Console.WriteLine("Enter device/computer name: ");
+    string computerName = new String(Console.ReadLine());
 
-      string computerName = new String(Console.ReadLine());
+    IPAddress[] ipAddresses = Dns.GetHostAddresses(computerName);
 
-      IPAddress[] ipAddresses = Dns.GetHostAddresses(computerName);
-
-      Console.WriteLine("IPv6: " + ipAddresses[0] + "\nIPv4: " + ipAddresses[1]);
-      Console.ReadKey();
-    }
-    catch(SocketException){
-      Console.WriteLine("No such host is known\nPress 'y' key to try again");
-      if(Console.ReadKey().KeyChar == 'y'){
-        Console.WriteLine();
-        DisplayData();
-      }
-    }
-}
-
-DisplayData();
+    Console.WriteLine("IPv6: " + ipAddresses[0] + "\nIPv4: " + ipAddresses[1]);
+    Console.ReadKey();
+  }
+  catch(SocketException){
+    Console.WriteLine("No such host is known\n");
+  }
+  catch(Exception ex){
+    Console.WriteLine("Unknown Error Occured");
+    Console.WriteLine(ex.ToString());
+  }
